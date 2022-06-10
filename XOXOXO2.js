ff5ae6d73dde68b826a890f61fb328164dcc2c67
sleepingontheriver <wotsit2001@gmail.com>
2022-06-10 20:52:49 +0100

    Initial commit

---
+++ XOXOXO2.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let StartingPosition = ["*", "*", "*",
+    "*", "*", "*",
+    "*", "*", "*"];
+    const Cross = "X"
+    const Nought = "O"
+    const Squares = document.querySelectorAll('.grid div')
+    const Reset = document.querySelector('button')
+
+    let turn = Cross
+
+    for (let i = 0; i < Squares.length; i++) {
+        Squares[i].onclick = () => {
+            Squares[i].classList.add('taken')
+            Squares[i].classList.add(turn)
+            if (turn == Cross){
+                turn = Nought
+                Squares[i].innerHTML = "<img src = 'X.png'/>"
+            }
+            else{
+                turn = Cross
+                Squares[i].innerHTML = "<img src = 'O.png'/>"
+            }
+        } 
+    }
+    Reset.onclick = () => {
+        document.querySelectorAll('.grid div').forEach(element => {
+            element.innerHTML = ""
+            element.classList.remove('taken', 'X', 'O')
+        })
+
+        }
+    
+})
