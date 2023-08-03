d6968b72b73852f716faa6007dbc7c01b1c6b9ef
sleepingontheriver <88553747+sleepingontheriver@users.noreply.github.com>
2023-08-03 13:06:00 +0100

    Add files via upload

--- XOXOXO2.js
+++ XOXOXO2.js
@@ -7,18 +7,71 @@ document.addEventListener('DOMContentLoaded', () => {
 
     let turn = Cross
 
+    let arr = [[""],[""],[""],[""],[""],[""],[""],[""],[""]];
+
+    function gameover(){
+        console.log("Game over")
+    }
+
     for (let i = 0; i < Squares.length; i++) {
         Squares[i].onclick = () => {
-            Squares[i].classList.add('taken')
+            
             Squares[i].classList.add(turn)
-            if (turn == Cross){
-                turn = Nought
-                Squares[i].innerHTML = "<img src = 'X.png'/>"
+
+            let id = Squares[i].id
+
+            arr[i] = turn
+
+            if (!(Squares[i].classList.contains("taken"))){
+                if (turn == Cross){
+                    turn = Nought
+                    Squares[i].innerHTML = "<img src = 'X.png'/>"
+                }
+                else{
+                    turn = Cross
+                    Squares[i].innerHTML = "<img src = 'O.png'/>"
+                }
+                
+                Squares[i].classList.add('taken')
+            
+                // check the rows
+
+                if (arr[0] == arr[1] && arr[0] == arr[2]){
+                    gameover()
+                }
+
+                if (arr[3] == arr[4] && arr[3] == arr[5]){
+                    gameover()
+                }
+                
+                if (arr[6] == arr[7] && arr[6] == arr[8]){
+                    gameover()
+                }
+
+                //then the columns and diags 
+
+                if (arr[0] == arr[3] && arr[0] == arr[6]){
+                    gameover()
+                }
+
+                if (arr[1] == arr[4] && arr[1] == arr[7]){
+                    gameover()
+                }
+
+                if (arr[2] == arr[5] && arr[2] == arr[8]){
+                    gameover()
+                }
+
+                if (arr[0] == arr[4] && arr[0] == arr[8]){
+                    gameover()
+                }
+
+                if (arr[6] == arr[4] && arr[6] == arr[2]){
+                    gameover()
+                }
+
             }
-            else{
-                turn = Cross
-                Squares[i].innerHTML = "<img src = 'O.png'/>"
-            }
+
         } 
     }
     Reset.onclick = () => {
@@ -26,7 +79,8 @@ document.addEventListener('DOMContentLoaded', () => {
             element.innerHTML = ""
             element.classList.remove('taken', 'X', 'O')
         })
-
+        turn = Cross
+        arr = [[""],[""],[""],[""],[""],[""],[""],[""],[""]]
         }
     
 })
